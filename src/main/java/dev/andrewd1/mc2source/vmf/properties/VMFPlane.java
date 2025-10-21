83f71e193fb48081ca5d71f241dc7e307df63a94
Andrew <andrew@andrewd1.dev>
2025-10-21 14:23:47 -0700

    Revert "still pain but hopefully less but idk"
    This reverts commit 6f7ffcaf6b54c9dcc9243010ceadda3f2b83a5bf.

--- src/main/java/dev/andrewd1/mc2source/vmf/properties/VMFPlane.java
+++ src/main/java/dev/andrewd1/mc2source/vmf/properties/VMFPlane.java
@@ -9,12 +9,11 @@ import static dev.andrewd1.mc2source.vmf.VMFUtil.*;
 
 public class VMFPlane extends VMFProperty<Triple<Vector, Vector, Vector>> {
     public VMFPlane(String name, Block block, BlockFace face) {
-        this(
-                name,
+        super(name, new Triple<>(
                 minecraftToSourceLocation(block.getLocation().toVector().clone().add(getFirstPoint(face))),
                 minecraftToSourceLocation(block.getLocation().toVector().clone().add(getSecondPoint(face))),
                 minecraftToSourceLocation(block.getLocation().toVector().clone().add(getThirdPoint(face)))
-        );
+        ));
     }
     
     public VMFPlane(String name, Vector p1, Vector p2, Vector p3) {
@@ -23,37 +22,37 @@ public class VMFPlane extends VMFProperty<Triple<Vector, Vector, Vector>> {
 
     private static Vector getFirstPoint(BlockFace face) {
         return switch (face) {
-            case UP -> new Vector(0, 1, 0);  // bad
+            case UP -> new Vector(0, 1, 1);
             case DOWN -> new Vector(0, 0, 0);
-            case NORTH -> new Vector(0, 0, 0);  // bad
-            case SOUTH -> new Vector(0, 0, 1);
-            case EAST -> new Vector(1, 0, 0);  // bad
-            case WEST -> new Vector(0, 0, 0);
+            case NORTH -> new Vector(0, 1, 0);
+            case SOUTH -> new Vector(1, 1, 1);
+            case EAST -> new Vector(1, 1, 0);
+            case WEST -> new Vector(0, 1, 1);
             default -> new Vector(0, 0, 0);
         };
     }
 
     private static Vector getSecondPoint(BlockFace face) {
         return switch (face) {
-            case UP -> new Vector(1, 1, 0);  // bad
+            case UP -> new Vector(1, 1, 1);
             case DOWN -> new Vector(1, 0, 0);
-            case NORTH -> new Vector(1, 0, 0);  // bad
-            case SOUTH -> new Vector(1, 0, 1);
-            case EAST -> new Vector(1, 0, 1);  // bad
+            case NORTH -> new Vector(1, 1, 0);
+            case SOUTH -> new Vector(0, 1, 1);
+            case EAST -> new Vector(1, 1, 1);
             case WEST -> new Vector(0, 0, 1);
-            default -> new Vector(0, 0, 0);
+            default -> new Vector(1, 0, 0);
         };
     }
 
     private static Vector getThirdPoint(BlockFace face) {
         return switch (face) {
-            case UP -> new Vector(1, 1, 1);  // bad
+            case UP -> new Vector(1, 1, 0);
             case DOWN -> new Vector(1, 0, 1);
-            case NORTH -> new Vector(1, 1, 0);  // bad
-            case SOUTH -> new Vector(1, 1, 1);
-            case EAST -> new Vector(1, 1, 1);  // bad
-            case WEST -> new Vector(0, 1, 1);
-            default -> new Vector(0, 0, 0);
+            case NORTH -> new Vector(1, 0, 0);
+            case SOUTH -> new Vector(0, 0, 1);
+            case EAST -> new Vector(1, 0, 1);
+            case WEST -> new Vector(0, 0, 0);
+            default -> new Vector(1, 0, 1);
         };
     }
 
